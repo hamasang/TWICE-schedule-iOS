f29537770cc1660388099cc461660df6625f12ec
Min DaeHong <hamasang@mindaehong-ui-macbookpro.local>
2018-12-16 13:56:49 +0900

    First Commit

--- TWICE schedule/ViewController.m
+++ TWICE schedule/ViewController.m
@@ -17,6 +17,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *query=nil;
+    NSData *data=[NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://twiceapi.ialpha.kr/schedule/?token=ios"]];
+    NSError *error=nil;
+    id response=[NSJSONSerialization JSONObjectWithData:data options:
+                 NSJSONReadingMutableContainers | NSJSONReadingMutableLeaves error:&error];
+    
+    if (error) {
+        NSLog(@"%@",[error localizedDescription]);
+        query = @"OFF - 네트워크 상태를 확인해주세요.";
+    } else {
+        query = [NSString stringWithFormat:@"Schedule: %@ - %@", [self ScheduleOptions:[response objectForKey:@"event"]], [response objectForKey:@"Data"]];
+        NSLog(@"%@",query);
+    }
+    [_ScheduleDisplay setText:query];
+    
+}
+
+- (NSString*) ScheduleOptions:(NSString*)strings{
+    if([strings isEqualToString:@"O"]){
+        strings = @"OFF";
+    }else if([strings isEqualToString:@"T"]){
+        strings = @"TV";
+    }else if([strings isEqualToString:@"R"]){
+        strings = @"RADIO";
+    }else if([strings isEqualToString:@"E"]){
+        strings = @"ETC";
+    }else if([strings isEqualToString:@"C"]){
+        strings = @"CONCERT";
+    }else if([strings isEqualToString:@"M"]){
+        strings = @"Magazine";
+    }
+    return strings;
 }
 
 
